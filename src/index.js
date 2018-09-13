2b2645377ccea6c8416894f3ccb8486f8880041f
Siarhei Kvachonak <segrey1989@tut.by>
2018-09-13 12:01:19 +0300

    quadratic-equation: implemented solution. All tests done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var answer = [];
+  // убираю из строки все, кроме чисел с соответствующим знаком
+   var str = equation.replace(/\s/g, '').replace("*x^2", " ").replace("*x", " ");
+   //создаю массив чисел(был массив строк)
+   var arr = str.split(" ");
+    arr = arr.map(function(num) {
+  return Number(num);
+});
+
+var D = Math.pow(arr[1],2) - 4* arr[0] *arr[2];
+if(D > 0){
+answer.push(Math.round((arr[1]*(-1) + Math.sqrt(D)) / (2*arr[0])));
+answer.push(Math.round((arr[1]*(-1) - Math.sqrt(D)) / (2*arr[0])));
+ }
+ else if(D === 0)
+  answer.push(Math.round((arr[1]*(-1)) / (2*arr[0])));
+
+ return answer.sort(function(a,b){return a-b;});
+
 }
